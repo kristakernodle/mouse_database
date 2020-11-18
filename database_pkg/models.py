44e2bb94f2e16109897a2833150f3267cbc73536
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-11-18 12:27:34 -0500

    Created new Base class with as_dict and remove_from_db methods

--- database_pkg/models.py
+++ database_pkg/models.py
@@ -1,6 +1,7 @@
 import database_pkg as dpk
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
+import sqlalchemy
 import uuid
 from pathlib import Path
 from database_pkg.utilities import check_if_sharedx_connected
@@ -8,7 +9,18 @@ from database_pkg.utilities import check_if_sharedx_connected
 db = dpk.db
 
 
-class Mouse(db.Model):
+class Base(db.Model):
+    __abstract__ = True
+
+    def as_dict(self, my_object):
+        return {key: value for key, value in sqlalchemy.inspect(my_object).dict.items() if '_sa_' not in key}
+
+    def remove_from_db(self, my_object):
+        db.session.delete(my_object)
+        db.session.commit()
+
+
+class Mouse(Base):
     __tablename__ = 'mice'
     mouse_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     eartag = db.Column(db.Integer, nullable=False, unique=True)
@@ -21,8 +33,16 @@ class Mouse(db.Model):
     def __repr__(self):
         return f"< Mouse {self.eartag} >"
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class Experiment(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class Experiment(Base):
     __tablename__ = 'experiments'
     experiment_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     experiment_dir = db.Column(db.String, nullable=False, unique=True)
@@ -38,8 +58,16 @@ class Experiment(db.Model):
     def __repr__(self):
         return f"< Experiment {self.experiment_name} >"
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class Reviewer(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class Reviewer(Base):
     __tablename__ = 'reviewers'
     reviewer_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     first_name = db.Column(db.String, nullable=False)
@@ -49,8 +77,16 @@ class Reviewer(db.Model):
 
     scored_folders = relationship("BlindFolder", backref="reviewers")
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class ParticipantDetail(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class ParticipantDetail(Base):
     __tablename__ = 'participant_details'
     detail_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     mouse_id = db.Column(UUID(as_uuid=True), db.ForeignKey('mice.mouse_id'), nullable=False)
@@ -60,8 +96,16 @@ class ParticipantDetail(db.Model):
     participant_dir = db.Column(db.String, nullable=False, unique=True)
     exp_spec_details = db.Column(db.JSON)
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class Session(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class Session(Base):
     __tablename__ = 'sessions'
     session_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     mouse_id = db.Column(UUID(as_uuid=True), db.ForeignKey('mice.mouse_id'), nullable=False)
@@ -73,8 +117,16 @@ class Session(db.Model):
     folders = relationship("Folder", backref="sessions")
     trials = relationship("Trial", backref="sessions")
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class Folder(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class Folder(Base):
     __tablename__ = 'folders'
     folder_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     session_id = db.Column(UUID(as_uuid=True), db.ForeignKey('sessions.session_id'), nullable=False)
@@ -85,8 +137,16 @@ class Folder(db.Model):
     trials = relationship("Trial", backref="folders")
     score_folders = relationship("BlindFolder", backref="folders")
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class BlindFolder(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class BlindFolder(Base):
     __tablename__ = 'blind_folders'
     blind_folder_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     folder_id = db.Column(UUID(as_uuid=True), db.ForeignKey('folders.folder_id'), nullable=False)
@@ -95,24 +155,20 @@ class BlindFolder(db.Model):
 
     blind_trials = relationship("BlindTrial", backref="blind_folders")
 
-    def as_dict(self):
-        return {
-            'blind_folder_id': str(self.blind_folder_id),
-            'folder_id': str(self.folder_id),
-            'reviewer_id': str(self.reviewer_id),
-            'blind_name': self.blind_name
-        }
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-    def remove_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
 
     def add_blind_trials(self, scored_blind_folder_path=None):
         if scored_blind_folder_path is None:
             reviewer = Reviewer.query.filter(Reviewer.reviewer_id == self.reviewer_id).first()
             scored_blind_folder_path = Path(reviewer.scored_dir).joinpath(
                 f"{self.blind_name}_{reviewer.first_name[0]}{reviewer.last_name[0]}.csv")
-        database_pkg.CRUD.crud.add_blind_trials_from_scored_blind_folder(scored_blind_folder_path)
+        dpk.CRUD.crud.add_blind_trials_from_scored_blind_folder(scored_blind_folder_path)
 
     def is_scored(self):
         if not check_if_sharedx_connected:
@@ -123,7 +179,7 @@ class BlindFolder(db.Model):
         return scored_file_path.exists()
 
 
-class Trial(db.Model):
+class Trial(Base):
     __tablename__ = 'trials'
     trial_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     experiment_id = db.Column(UUID(as_uuid=True), db.ForeignKey('experiments.experiment_id'), nullable=False)
@@ -135,8 +191,16 @@ class Trial(db.Model):
 
     scores = relationship("SRTrialScore", backref='trials')
 
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
 
-class BlindTrial(db.Model):
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
+
+
+class BlindTrial(Base):
     __tablename__ = 'blind_trials'
     blind_trial_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     blind_folder_id = db.Column(UUID(as_uuid=True), db.ForeignKey('blind_folders.blind_folder_id'))
@@ -145,12 +209,16 @@ class BlindTrial(db.Model):
     folder_id = db.Column(UUID(as_uuid=True), db.ForeignKey('folders.folder_id'), nullable=False)
     blind_trial_num = db.Column(db.Integer, nullable=False)
 
-    def remove_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
+
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
 
 
-class SRTrialScore(db.Model):
+class SRTrialScore(Base):
     __tablename__ = 'sr_trial_scores'
     trial_score_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     trial_id = db.Column(UUID(as_uuid=True), db.ForeignKey('trials.trial_id'), nullable=False, unique=False)
@@ -158,3 +226,11 @@ class SRTrialScore(db.Model):
     reach_score = db.Column(db.Integer, nullable=False, unique=False)
     abnormal_movt_score = db.Column(db.Boolean, nullable=False, unique=False)
     grooming_score = db.Column(db.Boolean, nullable=False, unique=False)
+
+    def as_dict(self, my_object=None):
+        if my_object is None:
+            my_object = self
+        return super().as_dict(my_object)
+
+    def remove_from_db(self, my_object=None):
+        super().remove_from_db(my_object=self)
