b93f716ee4f0871d9284ae76608ca64909841a0b
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2021-02-12 09:01:11 -0500

    Transition analysis complete

--- data_visualization/grooming/transitions.py
+++ data_visualization/grooming/transitions.py
@@ -76,10 +76,10 @@ bouts_sum.insert(bouts_sum.shape[1], 'Percent interrupted bouts',
 #   1. Average number of transitions per bout by genotype
 a_file_name = 'average_num_transitions_per_bout.jpeg'
 avg_trans_fig = sns.barplot(x='genotype',
-                 y='Average number of transitions per bout',
-                 data=bouts_sum,
-                 palette=palette,
-                 capsize=0.2)
+                            y='Average number of transitions per bout',
+                            data=bouts_sum,
+                            palette=palette,
+                            capsize=0.2)
 avg_trans_fig.set_xlabel('')
 avg_trans_fig.set_ylabel('Number of transitions')
 plt.savefig(str(Path(save_dir).joinpath(a_file_name)))
@@ -112,10 +112,10 @@ plt.close()
 #   4. Percent interrupted bouts
 a_file_name = 'percent_interrupted_bouts.jpeg'
 perc_interrupted_fig = sns.barplot(x='genotype',
-                            y='Percent interrupted bouts',
-                            data=bouts_sum,
-                            palette=palette,
-                            capsize=0.2)
+                                   y='Percent interrupted bouts',
+                                   data=bouts_sum,
+                                   palette=palette,
+                                   capsize=0.2)
 perc_interrupted_fig.set_xlabel('')
 perc_interrupted_fig.set_ylabel('Percent of bouts')
 plt.savefig(str(Path(save_dir).joinpath(a_file_name)))
@@ -124,11 +124,69 @@ plt.close()
 #   5. Percent incomplete bouts
 a_file_name = 'percent_incomplete_bouts.jpeg'
 perc_interrupted_fig = sns.barplot(x='genotype',
-                            y='Percent incomplete bouts',
-                            data=bouts_sum,
-                            palette=palette,
-                            capsize=0.2)
+                                   y='Percent incomplete bouts',
+                                   data=bouts_sum,
+                                   palette=palette,
+                                   capsize=0.2)
 perc_interrupted_fig.set_xlabel('')
 perc_interrupted_fig.set_ylabel('Percent of bouts')
 plt.savefig(str(Path(save_dir).joinpath(a_file_name)))
+plt.close()
+
+## Incorrect transition type by genotype
+all_incorrect_transitions = list()
+
+bout: dbpkg.GroomingBout
+for bout in experiment.grooming_bouts:
+
+    grooming_summary = dbpkg.GroomingSummary.query.get(bout.grooming_summary_id)
+    session = dbpkg.Session.query.get(bout.session_id)
+    mouse = dbpkg.Mouse.query.get(session.mouse_id)
+
+    if mouse.genotype:
+        genotype = 'Knock-Out'
+    else:
+        genotype = 'Control'
+
+    new_row = {'eartag': mouse.eartag,
+               'genotype': genotype,
+               'birthdate': mouse.birthdate,
+               'sex': mouse.sex,
+               'session_date': session.session_date,
+               'session_dir': session.session_dir,
+               'scored_session_dir': grooming_summary.scored_session_dir,
+               'trial_num': grooming_summary.trial_num,
+               'total_num_transitions': bout.total_num_transitions}
+
+    incorrect_transition_details = {
+        'Aborted': len(bout.aborted_transitions),
+        'Skipped': len(bout.skipped_transitions),
+        'Reversed': len(bout.reversed_transitions),
+        'Incorrect initiation': len(bout.initiation_incorrect_transitions)
+    }
+    for key in incorrect_transition_details.keys():
+        this_row = dict(**new_row,
+                        **{'Transition Type': key,
+                           'Number of Transitions': incorrect_transition_details[key]})
+        all_incorrect_transitions.append(this_row)
+
+all_incorrect_transitions_df = pd.DataFrame.from_records(all_incorrect_transitions)
+DATE = 20210211
+all_incorrect_transitions_df.to_csv(f"{save_dir}/all_incorrect_transitions_{DATE}.csv")
+
+all_incorrect_transitions_df.insert(all_incorrect_transitions_df.shape[1],
+                                    'Percent of incorrect transitions',
+                                    all_incorrect_transitions_df['Number of Transitions'] /
+                                    all_incorrect_transitions_df['total_num_transitions'] * 100)
+
+g = sns.catplot(data=all_incorrect_transitions_df,
+                kind='bar',
+                x="Transition Type",
+                y="Percent of incorrect transitions",
+                hue='genotype',
+                palette=palette,
+                legend=False,
+                capsize=0.2)
+plt.legend(title='Genotype')
+plt.savefig(str(Path(save_dir).joinpath('incorrect_transition_type_by_genotype.jpeg')), bbox_inches='tight')
 plt.close()
